a01b17fc5d14dc7fc228743f2c9f41334e18aa93
shu <shu@aryao.cn>
2019-08-06 08:25:54 +0800

    Update the import paths for Caddy

--- setup.go
+++ setup.go
@@ -3,8 +3,8 @@ package locale
 import (
 	"strings"
 
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 
 	"github.com/simia-tech/caddy-locale/method"
 )
--- setup_test.go
+++ setup_test.go
@@ -3,7 +3,7 @@ package locale
 import (
 	"testing"
 
-	"github.com/mholt/caddy"
+	"github.com/caddyserver/caddy"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
