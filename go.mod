24732f75859a1d08fbf6043c095268087e19573f
Philipp Brüll <pb@simia.tech>
2020-02-18 10:45:49 +0100

    added go mod and fixed imports

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/simia-tech/caddy-locale
+
+go 1.13
+
+require (
+	github.com/caddyserver/caddy v1.0.4
+	github.com/stretchr/testify v1.4.0
+)
--- middleware_test.go
+++ middleware_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
